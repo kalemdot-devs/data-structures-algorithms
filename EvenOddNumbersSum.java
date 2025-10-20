09ba938c8aef85322155d264e7129406d33a3000
Muhammad Kaleem <106500052+kalemdot-devs@users.noreply.github.com>
2025-10-20 07:38:45 +0500

    Add files via upload

---
+++ EvenOddNumbersSum.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class EvenOddNumbersSum {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter your numbers : ");
+        // int num = sc.nextInt();
+        // int evenSum = 0;
+        // int oddSum = 0;
+
+        // while (num > 0) {
+        //     if (num%2 == 0) {
+        //         evenSum += num;
+        //     } else {
+        //         oddSum += num;
+        //     }
+
+        //     num--;
+
+        // }
+
+        // System.out.println("sum of even numbers : " + evenSum);
+        // System.out.println("sum of odd numbers : " + oddSum);
+
+        // sc.close();
+        
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number count : ");
+        int n = sc.nextInt();
+        int evenSum = 0; 
+        int oddSum = 0;
+
+        for(int i=1; i<n; i++) {
+            System.out.println("enter your number : ");
+            int num = sc.nextInt();
+            if(num%2 == 0) {
+                evenSum += num;
+            } else {
+                oddSum += num;
+            }
+        }
+
+        sc.close();
+        System.out.println("sum of even numbers : " + evenSum);
+        System.out.println("sum of odd numbers : " + oddSum);
+    }
+}
+
+
+
+
